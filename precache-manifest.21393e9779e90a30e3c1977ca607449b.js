33d15f6dadfa2374d316234bcc78f84fa2485c67
Kuljeet-123 <kuljeetsingh951@gmail.com>
2020-05-02 13:22:24 +0530

    Updates

---
+++ precache-manifest.21393e9779e90a30e3c1977ca607449b.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8907123744391ec3e4a9f40503a8c2a8",
+    "url": "/React-Redux-ToDo/index.html"
+  },
+  {
+    "revision": "21084f2a9d8119cbceda",
+    "url": "/React-Redux-ToDo/static/js/2.2ce6855b.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/React-Redux-ToDo/static/js/2.2ce6855b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "50858d91b479ea286f97",
+    "url": "/React-Redux-ToDo/static/js/main.97f771d9.chunk.js"
+  },
+  {
+    "revision": "6f8d299e5c5ae0846c46",
+    "url": "/React-Redux-ToDo/static/js/runtime-main.acd8dd42.js"
+  },
+  {
+    "revision": "441d8611106ab6473ed8c5487cda63ac",
+    "url": "/React-Redux-ToDo/static/media/cross.441d8611.svg"
+  },
+  {
+    "revision": "d99368ca4e133a9917838e76243e8cfb",
+    "url": "/React-Redux-ToDo/static/media/facebook.d99368ca.svg"
+  },
+  {
+    "revision": "192a6620a687a9e7e51c0037973a1547",
+    "url": "/React-Redux-ToDo/static/media/github.192a6620.svg"
+  },
+  {
+    "revision": "5b23a837268b09d0efa9e307023c1dae",
+    "url": "/React-Redux-ToDo/static/media/instagram.5b23a837.svg"
+  },
+  {
+    "revision": "9e36c8fd15d6f93ed6a45f9484746e8f",
+    "url": "/React-Redux-ToDo/static/media/linkedin.9e36c8fd.svg"
+  },
+  {
+    "revision": "6776b87f65afa3888ffbb437dcea36f7",
+    "url": "/React-Redux-ToDo/static/media/moon.6776b87f.svg"
+  },
+  {
+    "revision": "a19509c43a24df65ac61cd7714e967d3",
+    "url": "/React-Redux-ToDo/static/media/pencil.a19509c4.svg"
+  },
+  {
+    "revision": "f27d5f1c568abfc4ffee2a329b26c3ed",
+    "url": "/React-Redux-ToDo/static/media/save.f27d5f1c.svg"
+  },
+  {
+    "revision": "37c6455fd06185db8ea46a4289c6ca91",
+    "url": "/React-Redux-ToDo/static/media/success.37c6455f.svg"
+  },
+  {
+    "revision": "e169a38be874bc5abd902d9d277f82f3",
+    "url": "/React-Redux-ToDo/static/media/sun.e169a38b.svg"
+  },
+  {
+    "revision": "480b19cb87fb335bbaed8dc9bd93a2c3",
+    "url": "/React-Redux-ToDo/static/media/trash.480b19cb.svg"
+  }
+]);
